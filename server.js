8f37ced75ee95ad3282bd5f4857b63db35e57b4b
mattlawson80 <matt.lawson@i3solutions.com>
2016-01-10 19:18:25 -0500

    Add POST for todos

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-		id: 1,
-		description: 'Meet mom for lunch',
-		completed: false
-	}, {
-		id: 2,
-		description: 'Go to market',
-		completed: false
-	},
-	{
-		id: 3,
-		description: 'Feed the cat',
-		completed: true
-	}
-];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -40,7 +31,6 @@ app.get('/todos/:id', function (req, res)
 		}
 	});
 
-	console.log(foundTodo);
 	if (foundTodo)
 	{
 		res.json(foundTodo);
@@ -49,6 +39,18 @@ app.get('/todos/:id', function (req, res)
 	}
 
 	
+});
+
+// POST /todos
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	body.id = todoNextId;
+	todoNextId++;
+
+	todos.push(body);
+
+	res.json(body);
 });
 
 app.listen(PORT, function() {
